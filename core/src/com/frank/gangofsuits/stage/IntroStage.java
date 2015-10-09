e8d4de1706f9f4c70db1440471a62ed2d2a45af4
Frank Paschen <frankpaschen99@gmail.com>
2015-10-09 16:16:10 -0400

    Camera work

--- core/src/com/frank/gangofsuits/stage/IntroStage.java
+++ core/src/com/frank/gangofsuits/stage/IntroStage.java
@@ -1,11 +1,12 @@
 package com.frank.gangofsuits.stage;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.utils.viewport.FitViewport;
-import com.badlogic.gdx.utils.viewport.Viewport;
 import com.frank.gangofsuits.GangOfSuits;
 import com.frank.gangofsuits.player.Player;
 import com.frank.gangofsuits.utilities.Constants;
@@ -13,22 +14,41 @@ import com.frank.gangofsuits.utilities.Constants;
 public class IntroStage implements Screen {
 	private Player player;
 	private TiledMap map;
-	private Camera camera;
-	private Viewport viewport;
 	private GangOfSuits game;
+	private SpriteBatch batch;
+	
+	// Sprite gives us more capabilities than Texture
+	private Sprite background;
+	
+	// Camera+Viewport
+	OrthographicCamera camera;
 	
 	public IntroStage(GangOfSuits game) {
 		this.game = game;
-		this.viewport = new FitViewport(Constants.WORLD_WIDTH, Constants.WORLD_HEIGHT);
-		this.camera = new OrthographicCamera();
+		batch = new SpriteBatch();
+		
+		background = new Sprite(new Texture(Gdx.files.internal("map.png")));
+		background.setSize(Constants.WORLD_WIDTH, Constants.WORLD_HEIGHT);
+		
+		float aspectRatio = (float) Gdx.graphics.getHeight() / (float) Gdx.graphics.getHeight();
+		
+		camera = new OrthographicCamera(Constants.WORLD_WIDTH * aspectRatio, Constants.WORLD_HEIGHT * aspectRatio);
+		camera.translate(camera.viewportWidth / 2, camera.viewportHeight / 2);
 	}
 	@Override
 	public void show() {
 
 	}
 	@Override
-	public void render(float delta) {
-
+	public void render(float delta) {	
+		camera.update();
+		
+		batch.begin();
+		
+		batch.setProjectionMatrix(camera.combined);
+		batch.draw(background, 0, 0);
+		
+		batch.end();
 	}
 	@Override
 	public void resize(int width, int height) {
--- core/src/com/frank/gangofsuits/utilities/Constants.java
+++ core/src/com/frank/gangofsuits/utilities/Constants.java
@@ -1,6 +1,6 @@
 package com.frank.gangofsuits.utilities;
 
 public final class Constants {
-	public static final float WORLD_WIDTH = 1000;
-	public static final float WORLD_HEIGHT = 1000;
+	public static final float WORLD_WIDTH = 512;
+	public static final float WORLD_HEIGHT = 512;
 }
