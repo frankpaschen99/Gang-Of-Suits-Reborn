6cd72177e2fe17a4656a0cc06c1d4b6f6bfbde00
Frank Paschen <frankpaschen99@gmail.com>
2015-10-09 00:51:54 -0400

    Added constructor and movement functions to player.java

--- core/src/com/frank/gangofsuits/player/Player.java
+++ core/src/com/frank/gangofsuits/player/Player.java
@@ -1,5 +1,40 @@
 package com.frank.gangofsuits.player;
 
-public class Player {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+public class Player {
+	private Texture texture;
+	private Sprite sprite;
+	
+	public Player() {
+		texture = new Texture(Gdx.files.internal("char_sprite.png"));
+		sprite = new Sprite(texture);
+		sprite.setPosition(0, 0);
+		sprite.setRotation(0);
+	}
+	
+	public void draw(SpriteBatch batch) {
+		batch.begin();
+		sprite.draw(batch);
+		batch.end();
+	}
+	
+	public void moveUp() {
+		
+	}
+	
+	public void moveDown() {
+		
+	}
+	
+	public void moveLeft() {
+		
+	}
+	
+	public void moveRight() {
+		
+	}
 }
