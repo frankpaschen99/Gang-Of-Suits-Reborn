64e15d9e567d971819ae3b52d32f19c79a69b5ee
Frank Paschen <frankpaschen99@gmail.com>
2015-10-09 23:47:01 -0400

    tweaked world dimensions again

--- core/src/com/frank/gangofsuits/utilities/Constants.java
+++ core/src/com/frank/gangofsuits/utilities/Constants.java
@@ -2,5 +2,5 @@ package com.frank.gangofsuits.utilities;
 
 public final class Constants {
 	public static final float WORLD_WIDTH = 340;
-	public static final float WORLD_HEIGHT = 340;
+	public static final float WORLD_HEIGHT = 225;
 }
