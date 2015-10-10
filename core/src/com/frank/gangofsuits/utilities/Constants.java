963f07c6f070307492988f36bba70287f95ca25f
Frank Paschen <frankpaschen99@gmail.com>
2015-10-09 23:42:10 -0400

    Made world size smaller to make sprite larger

--- core/src/com/frank/gangofsuits/utilities/Constants.java
+++ core/src/com/frank/gangofsuits/utilities/Constants.java
@@ -1,6 +1,6 @@
 package com.frank.gangofsuits.utilities;
 
 public final class Constants {
-	public static final float WORLD_WIDTH = 512;
-	public static final float WORLD_HEIGHT = 512;
+	public static final float WORLD_WIDTH = 340;
+	public static final float WORLD_HEIGHT = 340;
 }
