89880f30e404d1a73da9cdb7d900c6cf57180f41
Frank Paschen <frankpaschen99@gmail.com>
2015-10-17 02:34:09 -0400

    added difficulty option

--- core/src/com/frank/gangofsuits/utilities/Constants.java
+++ core/src/com/frank/gangofsuits/utilities/Constants.java
@@ -4,4 +4,5 @@ public final class Constants {
 	public static final String GAME_VERSION = "0.0.2";
 	public static final float WORLD_WIDTH = 340;
 	public static final float WORLD_HEIGHT = 225;
+	public static final int DIFFICULTY = 1;	// 1 = normal, 2 = hard
 }
